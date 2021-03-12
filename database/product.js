6054aaf3b79c1d919f1a299fdd0bce055ee20d54
Piyamin <piyaminjaima42@gmail.com>
2021-03-12 09:15:49 +0700

    db product ex

---
+++ database/product.js
@@ -0,0 +1,16 @@
+product =[
+    {
+    "product_id":"1",
+    "product_name":"Not",
+    "image_path":"",
+    "price":"",
+    "detail":""
+    },
+    {
+        "product_id":"1",
+        "product_name":"Not",
+        "image_path":"",
+        "price":"",
+        "detail":""
+        }
+]
