d5d5946a8ce8a8cd195e6420523b87654c17c56d
chutima anantakul <chutimafernn@gmail.com>
2021-03-12 09:27:18 +0700

    data

--- database/product.js
+++ database/product.js
@@ -1,16 +1,72 @@
 product =[
     {
-    "product_id":"1",
-    "product_name":"Not",
-    "image_path":"",
-    "price":"",
-    "detail":""
+        "product_id":"1",
+        "product_name":"NOTEBOOK (โน้ตบุ๊ค) LENOVO LEGION 5 17IMH05H-81Y8006XTA",
+        "image_path":"https://cf.shopee.co.th/file/51b653141eb466bb9b05eeefe7b82823",
+        "price":"42900",
+        "detail":"CPU : INTEL CORE I7-10750H RAM : 16 GB (8GB X2) DDR4 2933MHz STORAGE : 512 GB PCIe/NVMe M.2 SSD DISPLAY : 17.3 FULL HD IPS ANTI-GLARE 144Hz VGA : NVIDIA GEFORCE RTX2060 6 GB GDDR6 OS : WINDOWS 10 HOME"
     },
     {
-        "product_id":"1",
+        "product_id":"2",
+        "product_name":"SMARTPHONE (สมาร์ทโฟน) LENOVO LEGION PHONE DUEL L79031 TH (12GB/256GB) (BLUE)        ",
+        "image_path":"https://th-test-11.slatic.net/p/24408f9738cf5fb8e72b8cf6afe7a76f.jpg_720x720q80.jpg_.webp        ",
+        "price":"20490",
+        "detail":"OS : ANDROID 10 RAM : 12 GB DISPLAY : 6.65"
+    },
+    {
+        "product_id":"3",
+        "product_name":"NOTEBOOK (โน้ตบุ๊ค) ASUS ZENBOOK PRO DUO UX481FL-HJ096T (CELESTIAL BLUE)        ",
+        "image_path":"https://www.jib.co.th/img_master/product/original/2020062609204140825_1.jpg        ",
+        "price":"29400",
+        "detail":"CPU : INTEL CORE I5-10210U  RAM : 16 GB LPDDR3 (ON-BOARD) STORAGE : 512 GB PCIe/NVMe M.2 SSD DISPLAY : 14 FULL HD VGA : NVIDIA GEFORCE MX250 2 GB GDDR5 OS : WINDOWS 10"
+    },
+    {
+        "product_id":"4",
+        "product_name":"HEADPHONE (หูฟัง) RAZER HAMMERHEAD PRO V2 GAMING GEAR",
+        "image_path":"https://th-test-11.slatic.net/p/960e005c9a469cec3755976c77690fd2.jpg_720x720q80.jpg_.webp",
+        "price":"1290",
+        "detail":"IN-LINE REMOTE IS NOT WORKING WITH WINDOWS HEADSET RESPONSE : 20 Hz - 20000 Hz MIC RESPONSE : 50 Hz - 10000 Hz"
+    },
+    {
+        "product_id":"5",
+        "product_name":"CPU (ซีพียู) INTEL 1200 CORE I5-10400F 2.9 GHz",
+        "image_path":"https://cf.shopee.co.th/file/34c173f405b3e5ccff91ad6bb0bda830",
+        "price":"5090",
+        "detail":"CORES : 6 THREADS : 12 DISCRETE GRAPHICS REQUIRED, NO INTEGRATED GRAPHICS."
+    },
+    {
+        "product_id":"6",
+        "product_name":"SMARTPHONE (สมาร์ทโฟน) SAMSUNG GALAXY NOTE 20 (8GB/256GB) MYSTIC BRONZE",
+        "image_path":"https://www.bnn.in.th/pub/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/S/a/Samsung-Galaxy-Note-20-Mystic-Bronze-1-1614702300.jpg",
+        "price":"29900",
+        "detail":"Octa Core 2.73GHz 10MP Front 64MP+12MP+12MP Back 8GB / 256GB / 6.7 FHD+4G Dual SIM + eSIM / 4300mAh S-PEN"
+    },
+    {
+        "product_id":"2",
         "product_name":"Not",
         "image_path":"",
         "price":"",
         "detail":""
-        }
+    },
+    {
+        "product_id":"2",
+        "product_name":"Not",
+        "image_path":"",
+        "price":"",
+        "detail":""
+    },
+    {
+        "product_id":"2",
+        "product_name":"Not",
+        "image_path":"",
+        "price":"",
+        "detail":""
+    },
+    {
+        "product_id":"2",
+        "product_name":"Not",
+        "image_path":"",
+        "price":"",
+        "detail":""
+    },
 ]
